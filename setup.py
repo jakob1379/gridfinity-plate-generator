87b611c9921772bb99b12019233a87f453c47214
jga <jga@evaxion-biotech.com>
2023-08-31 22:34:52 +0200

    more pip

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='gridfinity-plate-generator',
+    name='gridfinity_plate_generator',
     version='0.1.0',
     description='Gridfinity Plate Generator',
     long_description=open('README.md').read(),
