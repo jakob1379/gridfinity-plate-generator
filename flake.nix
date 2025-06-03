175c3764062aa5c189d6ef253ce739d60e8a9cf6
Jakob Guldberg Aaes <jakob1379@gmail.com>
2025-06-03 12:28:42 +0200

    refactor: migrated to uv instead of poetry

--- flake.nix
+++ flake.nix
@@ -1,46 +1,28 @@
 {
-  description = "A Python development environment";
-
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
-
-  outputs = { self, nixpkgs }:
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
     let
-      forAllSystems = function:
-        nixpkgs.lib.genAttrs [ "x86_64-linux" ]
-          (system: function nixpkgs.legacyPackages.${system});
-
-      generalPackages = pkgs: with pkgs; [
-        nodejs
-        pre-commit
-        rich-cli
-        python311
-        libglibutil
-        libglvnd
-        xorg.libX11 # Add the X11 library
-        # vagrant
-      ];
-
-      pythonPackages = pkgs: with pkgs.python311Packages; [
-        pip
-        pip-tools
-      ];
-    in {
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell {
-          packages = (generalPackages pkgs) ++ (pythonPackages pkgs);
-
-          # Add libglvnd and libX11 to the LD_LIBRARY_PATH
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-            pkgs.stdenv.cc.cc
-            pkgs.libglvnd
-            pkgs.xorg.libX11 # Add the path for libX11
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            uv
           ];
 
+          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [
+            pkgs.xorg_sys_opengl
+            pkgs.libGL
+            pkgs.xorg.libX11
+          ]}:$LD_LIBRARY_PATH";
+          
+
           shellHook = ''
-            poetry install
-            export PS1="(deploy-shell 🚀) $PS1"
-          '';
+          '' ;
         };
-      });
-    };
+      }
+  );
+
 }
